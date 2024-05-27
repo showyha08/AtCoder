06c23db62a88047e58e80bb065cb40b17b0611c9
大場 祥哉 <over.and.uver@gmail.com>
2024-05-27 17:06:20 +0900

    add_340

--- src/main/kotlin/abc340/b.kt
+++ src/main/kotlin/abc340/b.kt
@@ -2,6 +2,13 @@ package abc340
 
 fun main() {
     val n = readln().toInt()
-    val a = readln().split(" ").map { it.toLong() }
-
+    val ans = mutableListOf<Int>()
+    repeat(n) {
+        val (q, xORk) = readln().split(" ").map { it.toInt() }
+        if (q == 1) {
+            ans.add(xORk)
+        } else {
+            println(ans[ans.size - xORk])
+        }
+    }
 }
--- src/main/kotlin/abc340/c.kt
+++ src/main/kotlin/abc340/c.kt
@@ -1,7 +1,29 @@
 package abc340
 
+// メモ化再起
+var memo = mutableMapOf<Long, Long>()
+
+fun splitNum(i: Long): Long {
+    var ans = 0L
+    if (i in memo) {
+        return memo[i]!!
+    }
+
+    if (2 <= i) {
+        val divide1 = i / 2
+        val divide2 = (i + 1) / 2
+        val divide1Result = splitNum(divide1)
+        val divide2Result = splitNum(divide2)
+        ans = i + divide1Result + divide2Result
+        memo[i] = ans
+        return ans
+    } else {
+        return ans
+    }
+}
+
 fun main() {
-    val (h, w, n) = readln().split(" ").map { it.toInt() }
-    val t = readln().toCharArray()
-    val s = Array(h) { CharArray(w) }
+    val n = readln().toLong()
+    val ans = splitNum(n)
+    println(ans)
 }
